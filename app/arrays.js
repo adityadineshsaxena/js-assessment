8d9ef5cf29b70720b392d1d0ffb764693aa6b6e7
Aditya Saxena <Aditya_Saxena@mckinsey.com>
2015-05-25 20:28:31 +0530

    solution for first 4 Array tests

--- app/arrays.js
+++ app/arrays.js
@@ -3,19 +3,66 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    var index;
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i] === item) {
+        index = i;
+        break;
+      }
+      else {
+        index = -1;
+      }
+    }
+    return index;
   },
 
   sum : function(arr) {
-
+    var sum = 0;
+    for(var i = 0; i<arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
   },
 
   remove : function(arr, item) {
-
+    var tempArray = [];
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i] !== item) {
+        tempArray.push(arr[i]);
+      }
+    }
+    return tempArray;
   },
 
-  removeWithoutCopy : function(arr, item) {
+  removeWithoutCopy : function removeWithoutCopy(arr, item) {
+    var initLength = arr.length,
+    nullCount = 0;
+    for(var i = 0; i <arr.length; i++) {
+      if(arr[i] === item) {
+        arr[i] = null;
+        nullCount++;
+      }
+    }
 
+    function shiftNulls(arr, j) {
+      for(var i = j; i < arr.length; i++) {
+        arr[i] = arr[i+1]|| null;
+      }
+    }
+
+    var j = 0;
+    for(var k = 0; k < nullCount; k++) {
+      for(; j < arr.length; j++) {
+        if(!arr[j]) {
+          shiftNulls(arr, j);
+          arr[j] && j++;
+          break;
+        }
+      }
+    }
+
+    arr.length = initLength - nullCount;
+    return arr;
   },
 
   append : function(arr, item) {
